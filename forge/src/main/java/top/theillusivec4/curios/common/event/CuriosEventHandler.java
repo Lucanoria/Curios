e45ab2861e88cbd50a85ff32ad08e71fa3b45bc8
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2024-08-31 04:32:21 -0700

    Fix attribute modifier collisions

--- forge/src/main/java/top/theillusivec4/curios/common/event/CuriosEventHandler.java
+++ forge/src/main/java/top/theillusivec4/curios/common/event/CuriosEventHandler.java
@@ -602,7 +602,7 @@ public class CuriosEventHandler {
                     AttributeInstance attInst = attributeMap.getInstance(key);
 
                     if (attInst != null) {
-                      attInst.addTransientModifier(value);
+                      attInst.addOrUpdateTransientModifier(value);
                     }
                   });
                   handler.addTransientSlotModifiers(slots);
